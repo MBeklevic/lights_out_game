f3d829adcf4ed116c5e0aac792ee2e87f1334e19
MBeklevic <m.beklevic37@gmail.com>
2022-10-30 15:57:33 +0300

    Updates

---
+++ precache-manifest.0d853acb23840144740e6f0ea7ce456c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f8c3631526cb05c140d",
+    "url": "/lights_out_game/static/css/main.950e527c.chunk.css"
+  },
+  {
+    "revision": "8f8c3631526cb05c140d",
+    "url": "/lights_out_game/static/js/main.8f8c3631.chunk.js"
+  },
+  {
+    "revision": "9b314b03da5604077d84",
+    "url": "/lights_out_game/static/js/1.9b314b03.chunk.js"
+  },
+  {
+    "revision": "e0eaf5df294a509bac51",
+    "url": "/lights_out_game/static/js/runtime~main.e0eaf5df.js"
+  },
+  {
+    "revision": "7379c87e4d21179090f67b45eaace11b",
+    "url": "/lights_out_game/index.html"
+  }
+];
